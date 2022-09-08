b9543796540c969ff72924409da75516d216e32c
EsengulCinelJForce <esengul.cinel@jforce.com>
2022-09-08 23:18:29 +0300

    first commit for Spring Boot

---
+++ src/test/java/com/esengulcinel/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esengulcinel.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
