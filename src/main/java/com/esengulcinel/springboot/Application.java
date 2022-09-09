5f4b6c35af85c64e349d611b24d7a875ee9386cd
EsengulCinelJForce <esengul.cinel@jforce.com>
2022-09-09 13:25:32 +0300

    applicationSecurityExclude

--- src/main/java/com/esengulcinel/springboot/Application.java
+++ src/main/java/com/esengulcinel/springboot/Application.java
@@ -3,10 +3,14 @@ package com.esengulcinel.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {  //security'i dahil etme
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 
